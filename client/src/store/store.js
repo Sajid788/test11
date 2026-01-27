317b79539913d3c6301924d850a7d39d04e9b9a0
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2026-01-27 15:34:57 +0530

    Delete client directory

--- client/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import diceReducer from './diceSlice';
-
-export const store = configureStore({
-  reducer: {
-    dice: diceReducer,
-  },
-});
-
